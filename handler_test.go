d74d683d9906f38e278dd87f1cd1ff68b574fd21
Peter Aronoff <peter@aronoff.org>
2023-08-14 03:47:40 -0400

    fix: inline Attrs for a group with an empty key (#27)
    * fix: inline Attrs for a group with an empty key
    
    This fixes a bug where tint produces `.c=d` if the user passes an
    attribute like `slog.Group("", slog.String("c", "d"))`. This result
    violates one of slog's rules for handlers: "If a group's key is empty,
    inline the group's Attrs."
    
    * added test case comment
    
    * consistency
    
    ---------
    
    Co-authored-by: lmittmann <lmittmann@users.noreply.github.com>

--- handler_test.go
+++ handler_test.go
@@ -275,6 +275,12 @@ func TestHandler(t *testing.T) {
 			},
 			Want: `Nov 11 23:00:00.000 ERR test`,
 		},
+		{ // https://github.com/lmittmann/tint/pull/27
+			F: func(l *slog.Logger) {
+				l.Info("test", "a", "b", slog.Group("", slog.String("c", "d")), "e", "f")
+			},
+			Want: `Nov 10 23:00:00.000 INF test a=b c=d e=f`,
+		},
 	}
 
 	for i, test := range tests {
