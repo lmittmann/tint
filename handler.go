16b13f5dc28c17e33eb90f94d33dc7967ce86fe8
lmittmann <lmittmann@users.noreply.github.com>
2024-01-18 23:00:15 +0100

    fixed field name output

--- handler.go
+++ handler.go
@@ -390,7 +390,7 @@ func (h *handler) appendValue(buf *buffer, v slog.Value, quote bool) {
 		case *slog.Source:
 			h.appendSource(buf, cv)
 		default:
-			appendString(buf, fmt.Sprint(v.Any()), quote)
+			appendString(buf, fmt.Sprintf("%+v", v.Any()), quote)
 		}
 	}
 }
