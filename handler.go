a6918c7cf30ad11fab04df32c563957455262b9c
joris-bright <joris@bright-energy.eu>
2025-03-14 17:01:16 +0100

    chore: add Color description

--- handler.go
+++ handler.go
@@ -763,6 +763,14 @@ func Err(err error) slog.Attr {
 	return slog.Any(errKey, err)
 }
 
+// Color represents a color for tinting (colorizing) logs.
+//
+//	0-  7:  standard colors (as in ESC [ 30–37 m)
+//	8- 15:  high intensity colors (as in ESC [ 90–97 m)
+//
+// 16-231:  6 × 6 × 6 cube (216 colors): 16 + 36 × r + 6 × g + b (0 ≤ r, g, b ≤ 5)
+// 232-255:  grayscale from dark to light in 24 steps
+// see https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
 type Color uint8
 
 func (m Color) ansiCode() string {
